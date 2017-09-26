7b30b6780c97447b074ea5e64a34a5c07085ad00
nhannguyen95 <nhannguyenmath95@gmail.com>
2017-09-26 15:12:52 +0700

    univariate linear regression (data1)

---
+++ linear-regression/univariate.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+data = np.loadtxt('../data/data1.txt', delimiter=',')
+
+X = data[:, np.newaxis, 0]  # Population
+Y = data[:, np.newaxis, 1]  # Profit
+
+# Visualize data
+plt.figure()
+plt.grid()
+plt.xlabel('Population of City in 10,000s')
+plt.ylabel('Profit in $10,000s')
+plt.plot(X, Y, 'r+')
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model
+regr.fit(X, Y)
+
+# Make predictions
+Y_pred = regr.predict(X)
+
+# The coefficients
+print('Coefficients: %f %f\n' % (regr.intercept_, regr.coef_))
+
+# Plot output
+plt.plot(X, Y_pred, color='blue')
+plt.show()
